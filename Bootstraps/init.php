224f696825ee855901634c0e527ec99938daeb6c
Kenny macpro <trankennykhang@gmail.com>
2022-07-30 23:22:33 +0930

    add selenium command

--- Bootstraps/init.php
+++ Bootstraps/init.php
@@ -1,7 +1,8 @@
 <?php
 use Kupman\Scraper\Commands\PantherCommand;
+use Kupman\Scraper\Commands\SeleniumCommand;
 
 $application->add(new PantherCommand);
-
+$application->add(new SeleniumCommand);
 
 ?>
